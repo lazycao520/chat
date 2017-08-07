2bd24a96e2548dbed64c18aa503b1fc90dc0fb88
lazycao <lazycao@qq.com>
2017-08-07 16:41:05 +0800

    4

--- app.php
+++ app.php
@@ -2,25 +2,28 @@
 require 'vendor/autoload.php';
 $config = include('config/app.config.php');
 
-$redis = new Redis();
-$redis_client = $redis->getInstance();
+
 
 
 $server = new swoole_websocket_server($config['websocket']['address_id'], $config['websocket']['port']);
 
-$server->on('open', function (swoole_websocket_server $server, $request,$redis_client) {
+$server->on('open', function (swoole_websocket_server $server, $request) {
     echo "server: 有握手链接{$request->fd}\n";
 });
 
-$server->on('message', function (swoole_websocket_server $server, $frame,$redis_client) {
+$server->on('message', function (swoole_websocket_server $server, $frame) {
 //    echo "receive from {$frame->fd}:{$frame->data},opcode:{$frame->opcode},fin:{$frame->finish}\n";
     echo("有用户发送信息 \n");
     var_dump($frame);
+    $redis = new Redis();
+    $redis_client = $redis->getInstance();
     $redis_client->set('ST_'.$frame->fd,'yes');
 //    $server->push($frame->fd, "this is server");
 });
 
-$server->on('close', function ($ser, $fd,$redis_client) {
+$server->on('close', function ($ser, $fd) {
+    $redis = new Redis();
+    $redis_client = $redis->getInstance();
     $redis_client->del ('ST_'.$fd);
     echo "关闭链接 {$fd} closed\n";
 });
