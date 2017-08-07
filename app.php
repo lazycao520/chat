6ee9dc25ccd69f7f41914a9c0d7c80a5f38075e7
lazycao <lazycao@qq.com>
2017-08-07 17:20:42 +0800

    5

--- app.php
+++ app.php
@@ -3,8 +3,6 @@ require 'vendor/autoload.php';
 $config = include('config/app.config.php');
 
 
-
-
 $server = new swoole_websocket_server($config['websocket']['address_id'], $config['websocket']['port']);
 
 $server->on('open', function (swoole_websocket_server $server, $request) {
@@ -14,10 +12,31 @@ $server->on('open', function (swoole_websocket_server $server, $request) {
 $server->on('message', function (swoole_websocket_server $server, $frame) {
 //    echo "receive from {$frame->fd}:{$frame->data},opcode:{$frame->opcode},fin:{$frame->finish}\n";
     echo("有用户发送信息 \n");
-    var_dump($frame);
+    $data = $frame->data;
+    //开始数据
     $redis = new Redis();
     $redis_client = $redis->getInstance();
-    $redis_client->set('ST_'.$frame->fd,'yes');
+    $data = json_decode($data);
+    switch ($data['type']){
+        case 'start':{
+            if ($data['role'] == 'student'){
+                $redis_client->set('ST_'.$data['token'],$frame->fd);
+            }else{
+                $redis_client->set('TC_'.$data['token'],$frame->fd);
+            }
+            break;
+        }
+        case 'info':{
+            //获取教师fd
+            $teacher = $redis_client->get('TC_'.$data['teacher']);
+            if ($teacher){
+                $server->push($teacher, $data['message']);
+            }
+            break;
+        }
+    }
+
+
 //    $server->push($frame->fd, "this is server");
 });
 
